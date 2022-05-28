77b7889c2de298a1bbd37e6030fa77997ddf079b
Kuan Ting Pan <kuanthedev@gmail.com>
2022-05-28 23:41:36 +0800

    fix:collection cards layout

--- src/components/CollectionCard.jsx
+++ src/components/CollectionCard.jsx
@@ -1,11 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const CollectionCard = ({ background }) => {
+const CollectionCard = ({ background, slug }) => {
   return (
     <div
-      className="w-full h-full rounded-xl p-10"
-      style={{ backgroundImage: `url(${background})` }}
-    ></div>
+      className="w-[300px] h-[300px] rounded-xl p-10 cursor-pointer"
+      style={{
+        backgroundImage: `url(${background})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}
+    >
+      <Link to={slug} />
+    </div>
   );
 };
 
--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -9,21 +9,27 @@ import collection_f from "../assets/collection-f.png";
 
 const collections = {
   A: {
+    slug: "collection-a",
     background: collection_b,
   },
   B: {
+    slug: "collection-b",
     background: collection_b,
   },
   C: {
+    slug: "collection-c",
     background: collection_c,
   },
   D: {
+    slug: "collection-d",
     background: collection_d,
   },
   E: {
+    slug: "collection-e",
     background: collection_e,
   },
   F: {
+    slug: "collection-f",
     background: collection_f,
   },
 };
@@ -34,7 +40,7 @@ const explore = () => {
       <h1 className="text-6xl text-center font-bold mb-24">
         Explore Collections
       </h1>
-      <div className="w-full h-[600px] grid grid-rows-2 grid-cols-3 gap-4">
+      <div className="w-[1000px] h-[600px] mx-auto grid grid-rows-2 grid-cols-3 gap-10">
         {Object.values(collections).map((collection, index) => (
           <CollectionCard key={index} {...collection} />
         ))}
