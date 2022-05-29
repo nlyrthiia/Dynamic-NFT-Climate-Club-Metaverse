a7b975fd144e36e9e47656c955557028d920fc22
Kuan Ting Pan <kuanthedev@gmail.com>
2022-05-29 09:59:25 +0800

    ui: small changes to explore page

--- src/components/CollectionCard.jsx
+++ src/components/CollectionCard.jsx
@@ -1,18 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const CollectionCard = ({ background, slug }) => {
+const CollectionCard = ({ background, slug, name, creator, title, cot }) => {
   return (
-    <div
-      className="w-[300px] h-[300px] rounded-xl p-10 cursor-pointer"
-      style={{
-        backgroundImage: `url(${background})`,
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
-      }}
-    >
-      <Link to={slug} />
-    </div>
+    <Link to={slug} className="relative text-white">
+      <div
+        className="w-[300px] h-[300px] rounded-xl p-10 cursor-pointer flex flex-col justify-center items-center gap-2"
+        style={{
+          backgroundImage: `linear-gradient(135deg, rgba(120, 120, 120, 0.5), rgba(0, 0, 0, 0.5)), url(${background})`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+        }}
+      >
+        <h2 className="text-4xl">{name}</h2>
+        <p className="text-xs">Create by {creator}</p>
+        <h3 className="text-sm text-center">{title}</h3>
+      </div>
+      <div className="absolute bottom-2 right-4 text-[10px]">
+        total market cap : {cot}T
+      </div>
+    </Link>
   );
 };
 
--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,36 +1,60 @@
 import React from "react";
 
 import { CollectionCard } from "../components";
-import collection_b from "../assets/collection-b.png";
-import collection_c from "../assets/collection-c.png";
-import collection_d from "../assets/collection-d.png";
-import collection_e from "../assets/collection-e.png";
-import collection_f from "../assets/collection-f.png";
+import collection_b from "../assets/collectionB.png";
+import collection_c from "../assets/collectionC.png";
+import collection_d from "../assets/collectionD.png";
+import collection_e from "../assets/collectionE.png";
+import collection_f from "../assets/collectionA.png";
 
 const collections = {
   A: {
     slug: "collection-a",
     background: collection_b,
+    title: "Greentech Emissions Reductions",
+    name: "A",
+    creator: "F",
+    cot: 100,
   },
   B: {
     slug: "collection-b",
-    background: collection_b,
+    background: collection_c,
+    name: "B",
+    title: "Sichuan Biogas Stove Development Programme",
+    creator: "F",
+    cot: 100,
   },
   C: {
     slug: "collection-c",
-    background: collection_c,
+    background: collection_b,
+    name: "C",
+    creator: "F",
+    title: "Greentech Emissions Reductions",
+    cot: 100,
   },
   D: {
     slug: "collection-d",
     background: collection_d,
+    name: "D",
+    creator: "F",
+    title: "Solar Cooking for Refugee Families in Chad",
+    cot: 100,
   },
   E: {
     slug: "collection-e",
     background: collection_e,
+    name: "E",
+    creator: "F",
+    title: "Improved cookstoves in Maputo city, Mozambique",
+    cot: 100,
   },
   F: {
     slug: "collection-f",
     background: collection_f,
+    name: "F",
+    creator: "F",
+    title: "Lango Safe Water Project",
+    cot: 100,
   },
 };
 
