5356b180aea2c7d03c06db9e94d827373a341df0
kuan0808 <pp0923215826@gmail.com>
2022-06-01 20:44:59 +0000

    fix: minor issues

--- src/atoms/nftState.js
+++ src/atoms/nftState.js
@@ -1,5 +1,6 @@
 import { atom } from "recoil"
 import { initialNFTs } from "../data"
+
 export const ownedNFTsState = atom({
   key: "ownedNFTsState",
   default: [],
@@ -7,5 +8,5 @@ export const ownedNFTsState = atom({
 
 export const allNFTState = atom({
   key: "allNFTState",
-  default: [...initialNFTs],
+  default: [...initialNFTs.map(nft => ({...nft, cot: 8000, neutralized: false}))],
 })
